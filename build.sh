70b1497748fc80c9c33b00679038937586287174
Ar-Ray-code <ray255ar@gmail.com>
2022-12-11 03:36:28 +0900

    tmp 1211

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ docker run --rm -t -u $(id -u):$(id -g) \
   --mount=type=bind,source="$(pwd)/.ccache",destination=//.ccache \
   -w /var/mbed ghcr.io/armmbed/mbed-os-env \
   bash -c "mbed-tools deploy \
-    && mbed-tools compile -t GCC_ARM -m NUCLEO_F767ZI"
+    && mbed-tools compile -t GCC_ARM -m DISCO_F746NG"
--- workspace/main.cpp
+++ workspace/main.cpp
@@ -17,11 +17,18 @@
 #include "mbed.h"
 #include "mros2.h"
 #include "std_msgs/msg/string.hpp"
-#include "EthernetInterface.h"
+// #include "EthernetInterface.h"
+
+#include "WIZnetInterface.h"
+NetworkInterface *net;
+static WIZnetInterface eth1(D11, D12, D13, D6, D3);
+static uint8_t WIZNET_DEFAULT_TESTMAC[6] = {0x00, 0x08, 0xdc,
+                                            0x19, 0x85, 0xa8};
 
 #define IP_ADDRESS ("192.168.11.2") /* IP address */
 #define SUBNET_MASK ("255.255.255.0") /* Subnet mask */
 #define DEFAULT_GATEWAY ("192.168.11.1") /* Default gateway */
+#define DNS_SERVER ("8.8.8.8") /* DNS server */
 
 /* convert TARGET_NAME to put into message */
 #define quote(x) std::string(q(x))
@@ -33,11 +40,24 @@ void userCallback(std_msgs::msg::String *msg)
   printf("subscribed msg: '%s'\r\n", msg->data.c_str());
 }
 
+nsapi_size_or_error_t wiznet_init()
+{
+  eth1.init(WIZNET_DEFAULT_TESTMAC, IP_ADDRESS, SUBNET_MASK, DEFAULT_GATEWAY);
+  eth1.setDnsServerIP(DNS_SERVER);
+  net = &eth1;
+
+  return net->connect();
+}
+
 int main() {
-  EthernetInterface network;
-  network.set_dhcp(false);
-  network.set_network(IP_ADDRESS, SUBNET_MASK, DEFAULT_GATEWAY);
-  nsapi_size_or_error_t result = network.connect();
+  // serial begin
+  static BufferedSerial serial_port(USBTX, USBRX);
+  serial_port.set_baud(115200);
+  // EthernetInterface network;
+  // network.set_dhcp(false);
+  // network.set_network(IP_ADDRESS, SUBNET_MASK, DEFAULT_GATEWAY);
+  // nsapi_size_or_error_t result = network.connect();
+  nsapi_size_or_error_t result = wiznet_init();
 
   printf("mbed mros2 start!\r\n");
   printf("app name: echoback_string\r\n");
