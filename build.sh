859b7553875daf9b039d315f5824f77f64fa3e26
Ar-Ray-code <ray255ar@gmail.com>
2022-12-11 14:53:14 +0900

    remove build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm -t -u $(id -u):$(id -g) \
-  --mount=type=bind,source="$(pwd)",destination=/var/mbed \
-  --mount=type=bind,source="$(pwd)/.ccache",destination=//.ccache \
-  -w /var/mbed ghcr.io/armmbed/mbed-os-env \
-  bash -c "mbed-tools deploy \
-    && mbed-tools compile -t GCC_ARM -m NUCLEO_F767ZI"
